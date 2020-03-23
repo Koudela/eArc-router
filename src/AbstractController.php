f0e877457f62a67c2793683b75179d2b210a363b
Thomas Koudela <koudela@medienpark.net>
2020-03-23 14:53:14 +0100

    add: abstract controller

---
+++ src/AbstractController.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+/**
+ * e-Arc Framework - the explicit Architecture Framework
+ * router component
+ *
+ * @package earc/router
+ * @link https://github.com/Koudela/earc-router/
+ * @copyright Copyright (c) 2018-2020 Thomas Koudela
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace eArc\Router;
+
+use eArc\EventTree\Interfaces\PhaseSpecificListenerInterface;
+use eArc\EventTree\Interfaces\SortableListenerInterface;
+use eArc\EventTree\Transformation\ObserverTree;
+use eArc\Router\Interfaces\ControllerInterface;
+
+abstract class AbstractController implements ControllerInterface, PhaseSpecificListenerInterface, SortableListenerInterface
+{
+    public static function getPhase(): int
+    {
+        return ObserverTree::PHASE_DESTINATION;
+    }
+
+    public static function getPatience(): float
+    {
+        return 0;
+    }
+}
